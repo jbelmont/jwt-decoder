4b4fad72beeb0592c56e967a07af2e3168c85124
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2018-09-28 17:14:34 -0400

    Finish up rust program that decodes JWT.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,25 @@
 extern crate base64;
+extern crate serde;
+extern crate serde_json;
 
-use base64::{encode, decode};
+#[macro_use]
+extern crate serde_derive;
+
 use std::env;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub enum Algorithm {
-    HS256,
-    HS384,
-    HS512,
-    RS256,
-    RS384,
-    RS512,
-    ES256,
-    ES384,
-    ES512
-}
-
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct Header {
     typ: String,
-    alg: Algorithm,
+    alg: String,
 }
 
-pub struct Token<T> {
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct Token {
     header: Header,
-    claims: T,
-    signature: String,
+    claims: Claims,
 }
 
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct Claims {
     sub: String,
     iss: String,
@@ -37,11 +30,58 @@ pub struct Claims {
     jti: String,
 }
 
+impl Claims {
+    fn new() -> Self {
+        Default::default()
+    }
+}
+
+impl Header {
+    fn new() -> Self {
+        Default::default()
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let file = &args[1];
+    let mut header = Header::new();
+    let mut claims = Claims::new();
 
-    let decode_jwt = &decode(file).unwrap()[..];
-    println!("{:?}", decode_jwt);
+    let jwt_string: Vec<_> = args[1].split(".").collect();
+
+    let mut counter = 0;
+    for part in jwt_string {
+        if counter == 0 {
+            let decoded_header = base64::decode(part).unwrap();
+            let decoded_header = String::from_utf8(decoded_header).unwrap();
+            header = serde_json::from_str(&decoded_header).unwrap();
+        } else if counter == 1 {
+            let decoded_claims = base64::decode(part).unwrap();
+            let decoded_claims = String::from_utf8(decoded_claims).unwrap();
+            claims = serde_json::from_str(&decoded_claims).unwrap();
+        }
+        counter += 1;
+    }
+
+    let token = Token {
+        header: Header {
+            typ: header.typ,
+            alg: header.alg,
+        },
+        claims: Claims {
+            sub: claims.sub,
+            iss: claims.iss,
+            aud: claims.aud,
+            exp: claims.exp,
+            nbf: claims.nbf,
+            iat: claims.iat,
+            jti: claims.jti,
+        }
+    };
+    // Serialize it to a JSON string.
+    let serialize_jwt = serde_json::to_string(&token).unwrap();
+
+    // Print, write to a file, or send to an HTTP server.
+    println!("{:?}", serialize_jwt);
 }
